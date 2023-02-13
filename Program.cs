78e8f90688aef15f86a246f5b5a32b9024454836
denis <106389581+fishman123456@users.noreply.github.com>
2023-02-13 20:08:51 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿//using Microsoft.Data.Sql;
+using Microsoft.Data.SqlClient;
+
+//SqlConnection conn = null;
+string sqlSourse  = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Library; Integrated Security=true;";
+//Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = master; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False
+SqlConnection conn = new SqlConnection(sqlSourse);
+
+conn.Open();
+//Console.WriteLine("Введите имя автора");
+
+string insertString = @"insert into Authors Values ('44','ff')";
+
+SqlCommand insertdata = new SqlCommand(insertString, conn);
+insertdata.ExecuteNonQuery();
+
+conn.Close();
