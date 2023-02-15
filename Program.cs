1c91ba7f51a26c293db303fcef6aeed3d72d0fbb
denis <106389581+fishman123456@users.noreply.github.com>
2023-02-15 19:13:35 +0700

    безопасная передача параметров

--- Program.cs
+++ Program.cs
@@ -2,8 +2,11 @@
 using Microsoft.Data.SqlClient;
 
 //SqlConnection conn = null;
+
+// строка соединение
 string sqlSourse  = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Library; Integrated Security=true;";
 //Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = master; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False
+//открываем соединение
 SqlConnection conn = new SqlConnection(sqlSourse);
 
 conn.Open();
@@ -11,6 +14,32 @@ conn.Open();
 //string select = @"select* from Authors ";
 //string insertString = @"insert into Authors Values ('44','ff')";
 string insertString = @"insert into Books Values (1,'war',55,455)";
+
+int indexautor = 2;
+string bookname = "Oshen";
+int bookprise = 300;
+int page = 1205;
+// описываем каждый параметр
+
+SqlParameter param1 = new SqlParameter();
+param1.ParameterName = "AuthorId";
+param1.SqlDbType = System.Data.SqlDbType.VarChar;
+param1.Value = indexautor;
+
+SqlParameter param2 = new SqlParameter();
+param2.ParameterName = "Title";
+param2.SqlDbType = System.Data.SqlDbType.VarChar;
+param2.Value = bookname;
+
+SqlParameter param3 = new SqlParameter();
+param2.ParameterName = "PRICE";
+param2.SqlDbType = System.Data.SqlDbType.Int;
+param2.Value = bookprise;
+
+SqlParameter param4 = new SqlParameter();
+param2.ParameterName = "PAGES";
+param2.SqlDbType = System.Data.SqlDbType.Int;
+param2.Value = page;
 
 //SqlCommand insertdata = new SqlCommand(insertString, conn);
 
