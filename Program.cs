5187d3eda8dd13a9228e14ced911333d20edce81
denis <106389581+fishman123456@users.noreply.github.com>
2023-02-15 20:35:48 +0700

    добавили вывод всех авторов

--- Program.cs
+++ Program.cs
@@ -2,7 +2,6 @@
 using Microsoft.Data.SqlClient;
 
 //SqlConnection conn = null;
-
 // строка соединение
 string sqlSourse  = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=library; Integrated Security=true;";
 //Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = master; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False
@@ -10,52 +9,70 @@ string sqlSourse  = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=library
 SqlConnection conn = new SqlConnection(sqlSourse);
 
 conn.Open();
+
+SqlCommand cmd = new SqlCommand("getBooksNumber", conn);
+
+cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+cmd.Parameters.Add("@Authorid",System.Data.SqlDbType.Int).Value = 1;
+
+SqlParameter output = new SqlParameter("@BookCount", System.Data.SqlDbType.Int);
+
+output.Direction = System.Data.ParameterDirection.Output;
+
+cmd.Parameters.Add(output);
+
+cmd.ExecuteNonQuery();
+
+Console.WriteLine("У автора кол-во книг  "+
+
+cmd.Parameters["@BookCount"].Value.ToString());
+
+#region
 //Console.WriteLine("Введите имя автора");
 //string select = @"select* from Authors ";
 //string insertString = @"insert into Authors Values ('44','ff')";
 //string insertString = @"insert into Books Values (1,'war',55,455)";
-
-int indexautor = 1;
-string bookname = "Oshen";
-int bookprise = 300;
-int page = 1205;
-// описываем каждый параметр
-
-SqlParameter param1 = new SqlParameter();
-param1.ParameterName = "@AuthorId";
-param1.SqlDbType = System.Data.SqlDbType.Int;
-param1.Value = indexautor;
-
-SqlParameter param2 = new SqlParameter();
-param2.ParameterName = "@Title";
-param2.SqlDbType = System.Data.SqlDbType.VarChar;
-param2.Value = bookname;
-
-SqlParameter param3 = new SqlParameter();
-param3.ParameterName = "@PRICE";
-param3.SqlDbType = System.Data.SqlDbType.Int;
-param3.Value = bookprise;
-
-SqlParameter param4 = new SqlParameter();
-param4.ParameterName = "@PAGES";
-param4.SqlDbType = System.Data.SqlDbType.Int;
-param4.Value = page;
-
-string insertString = "insert into Books Values(@AuthorId,@Title,@PRICE,@PAGES)";
-SqlCommand insertdata = new SqlCommand(insertString, conn);
-insertdata.Parameters.AddRange(new SqlParameter[] { param1, param2, param3, param4 });
-Console.WriteLine(insertdata.ExecuteNonQuery());
-Console.ReadKey();
-//SqlCommand insertdata = new SqlCommand(insertString, conn);
-
+//Sqlcommand insertdata = new SqlCommand(insertString, conn);
 //SqlCommand comm = new SqlCommand(insertString, conn);
-
 //SqlDataReader reader = comm.ExecuteReader();
-
 //insertdata.ExecuteNonQuery();
 //string select = @"select* from Books ";
 //SqlCommand cmd = new SqlCommand(select, conn);
- //Console.WriteLine(" ");
+//Console.WriteLine(" ");
+#endregion
+#region
+//int indexautor = 1;
+//string bookname = "Oshen";
+//int bookprise = 300;
+//int page = 1205;
+//// описываем каждый параметр
 
-    conn.Close();
+//SqlParameter param1 = new SqlParameter();
+//param1.ParameterName = "@AuthorId";
+//param1.SqlDbType = System.Data.SqlDbType.Int;
+//param1.Value = indexautor;
+
+//SqlParameter param2 = new SqlParameter();
+//param2.ParameterName = "@Title";
+//param2.SqlDbType = System.Data.SqlDbType.VarChar;
+//param2.Value = bookname;
+
+//SqlParameter param3 = new SqlParameter();
+//param3.ParameterName = "@PRICE";
+//param3.SqlDbType = System.Data.SqlDbType.Int;
+//param3.Value = bookprise;
+
+//SqlParameter param4 = new SqlParameter();
+//param4.ParameterName = "@PAGES";
+//param4.SqlDbType = System.Data.SqlDbType.Int;
+//param4.Value = page;
+
+//string insertString = "insert into Books Values(@AuthorId,@Title,@PRICE,@PAGES)";
+//SqlCommand insertdata = new SqlCommand(insertString, conn);
+//insertdata.Parameters.AddRange(new SqlParameter[] { param1, param2, param3, param4 });
+//Console.WriteLine(insertdata.ExecuteNonQuery());
+//Console.ReadKey();
+#endregion
+conn.Close();
 
